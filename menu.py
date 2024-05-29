8f23338890ab5b756d8b04634da2b44859c4515b
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-05-29 12:43:09 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,20 +1,17 @@
 import streamlit as st
 
-
 def authenticated_menu():
     # Show a navigation menu for authenticated users
-    st.sidebar.page_link("Sign_In.py", label="Sign In/ Sign Up")
-    if st.session_state.role in ["admin", "user"]:
-        st.sidebar.page_link("pages/Home.py", label="Home")
-        st.sidebar.page_link("pages/1_General_Information.py", label="General Information")
-        st.sidebar.page_link("pages/Crop_Assist.py", label="Crop Assist")
-        st.sidebar.page_link("pages/Dimension_Manager.py", label="Dimension Manager")
-
+    st.sidebar.markdown("### Navigation")
+    st.sidebar.markdown("[Home](pages/Home.py)")
+    st.sidebar.markdown("[General Information](pages/1_General_Information.py)")
+    st.sidebar.markdown("[Crop Assist](pages/Crop_Assist.py)")
+    st.sidebar.markdown("[Dimension Manager](pages/Dimension_Manager.py)")
+    st.sidebar.markdown("[Sign Out](Sign_In.py)")
 
 def unauthenticated_menu():
     # Show a navigation menu for unauthenticated users
-    st.sidebar.page_link("Sign_In.py", label="Sign In/Sign Up")
-
+    st.sidebar.markdown("[Sign In/Sign Up](Sign_In.py)")
 
 def menu():
     # Determine if a user is logged in or not, then show the correct
@@ -24,10 +21,10 @@ def menu():
         return
     authenticated_menu()
 
-
 def menu_with_redirect():
     # Redirect users to the main page if not logged in, otherwise continue to
     # render the navigation menu
     if "role" not in st.session_state or st.session_state.role is None:
-        st.switch_page("Sign_In.py")
+        st.experimental_set_query_params(page="Sign_In")
+        st.experimental_rerun()
     menu()
