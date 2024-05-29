97ea8174158d087a1264e3900d0bb2d711b24812
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-05-29 13:27:45 +0200

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -2,22 +2,22 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import matplotlib.image as mpimg
 
-st.set_page_config(
-    page_title="Home page",
-    page_icon="👋",
-    layout="centered")
+def main():
+    st.set_page_config(
+        page_title="Home page",
+        page_icon="👋",
+        layout="centered")
 
+    # SurViZ logo
+    st.image('logo_ata.png.png')
 
-# SurViZ logo
-st.image('logo_ata.png.png')
-
-# Main Description
-st.markdown("## 👋 Welcome to the Dimension Detection tool!")
+    # Main Description
+    st.markdown("## 👋 Welcome to the Dimension Detection tool!")
     st.markdown("Developed by the Team Quadratech")
     st.markdown("The app is still under development.")
 
-# Description of the features. 
-st.markdown(
+    # Description of the features. 
+    st.markdown(
         """
         ### Select on the left panel what you want to explore:
 
@@ -26,6 +26,8 @@ st.markdown(
         - With 📈CropAssist, you will be able to Upload the drawings and get the dimensions.
 
         - With 🗺️ DimensionManager, you will be able to get all the detected Dimension organised into the corresponding lengths and widths.
-         """
+        """
+    )
 
-)
+if __name__ == "__main__":
+    main()
