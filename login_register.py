48e9131df6ded52620659019450f1b83b5d4c5b7
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-05-29 13:18:36 +0200

    Update login_register.py

--- login_register.py
+++ login_register.py
@@ -18,7 +18,7 @@ st.set_option("client.showSidebarNavigation", False)
 
 # Initialize st.session_state.logged_in and st.session_state.user if not already set
 if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
+    st.session_state.logged_in = True
 if "user" not in st.session_state:
     st.session_state.user = None
 
