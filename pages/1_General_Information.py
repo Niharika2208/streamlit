dbab1147cc5aa3f3b5bc8c0858c2c0bdebcba748
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-05-30 12:06:38 +0200

    Update 1_General_Information.py

--- pages/1_General_Information.py
+++ pages/1_General_Information.py
@@ -1,6 +1,9 @@
 import streamlit as st
 #from menu import menu
 
+st.sidebar.page_link("pages/1_General_Information.py", label="General Information")
+st.sidebar.page_link("pages/Crop_Assist.py", label="Crop Assist")
+st.sidebar.page_link("pages/Dimension_Manager.py", label="Dimension Manager")
 st.markdown('How to use the application?')
 
 #video_file = open('myvideo.mp4', 'rb')
