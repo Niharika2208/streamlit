bdb682526c6023bc70193df6d1b7de9e19cfa7a1
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-05-30 11:57:38 +0200

    Update Sign_In.py

--- Sign_In.py
+++ Sign_In.py
@@ -8,8 +8,8 @@ query_params = st.experimental_get_query_params()
 # Check if the 'page' parameter is set to 'home'
 if query_params.get("page") == ["home"]:
     # Load the Home page content
-    import pages.Home
-    pages.Home.main()  # Assuming you have a `main()` function in Home.py
+    from pages import Home
+    Home.main()  # Assuming you have a `main()` function in Home.py
 else:
     # Load the default login/register page
     import login_register  # Assuming this script is saved as login_register.py
