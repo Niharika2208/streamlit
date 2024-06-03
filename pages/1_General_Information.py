8dab65223f07320240cd9c81719a8b84e5bf7549
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-06-03 16:19:42 +0200

    Update 1_General_Information.py

--- pages/1_General_Information.py
+++ pages/1_General_Information.py
@@ -6,8 +6,8 @@ st.sidebar.page_link("pages/1_General_Information.py", label="General Informatio
 st.sidebar.page_link("pages/Crop_Assist.py", label="Crop Assist")
 st.sidebar.page_link("pages/Dimension_Manager.py", label="Dimension Manager")
 
-def app():
-  st.markdown('How to use the application?')
+
+st.markdown('How to use the application?')
 
 
 #video_file = open('myvideo.mp4', 'rb')
