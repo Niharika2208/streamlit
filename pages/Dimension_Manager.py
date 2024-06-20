d82f1982e305d41e759a2ff1c3e20bead5e766bf
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-06-20 11:44:57 +0200

    Update Dimension_Manager.py

--- pages/Dimension_Manager.py
+++ pages/Dimension_Manager.py
@@ -3,10 +3,7 @@ import streamlit as st
 def main():
     st.title("Dimension Manager")
     st.write("This is the Dimension Manager page.")
-    dimensions = st.sidebar.multiselect(
-    "Select the Dimension",
-     ["Length", "Width"]
-    )
+    
 
 if __name__ == "__main__":
     main()
