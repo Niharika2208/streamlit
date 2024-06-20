1f9101b447f5be325df40a54f857f21039c1762d
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-06-20 11:44:38 +0200

    Update Crop_Assist.py

--- pages/Crop_Assist.py
+++ pages/Crop_Assist.py
@@ -3,10 +3,7 @@ import streamlit as st
 def main():
     st.title("Crop Assist")
     st.write("This is the Crop Assist page.")
-    dimensions = st.sidebar.multiselect(
-    "Select the Dimension",
-     ["Length", "Width"]
-    )
+  
 
 if __name__ == "__main__":
     main()
